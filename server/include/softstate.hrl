9a04c39fa962f80e3294b82318a133b5f913b42b
Miguel <miguel.barreiro@miniclip.com>
2013-09-11 10:13:28 +0100

    -added unit testing to the project -adde combo detection to the game state

--- server/include/softstate.hrl
+++ server/include/softstate.hrl
@@ -21,21 +21,28 @@
 %----------------------
 
 -record( board, {
-	blocks = gb_tree:empty() :: gb_tree()
+	blocks = gb_trees:empty() :: gb_tree(),
+	width :: integer(),
+	height :: integer()
 }).
 
 -type block_type() :: color | garbage.
-
+-type color_type() :: red | yellow | blue | green.
 
 -record( block, {
-	type = color :: block_type()
+	type = color :: block_type(),
+	color :: color_type(),
+	x = 0 :: integer(),
+	y = 0 :: integer()
 }).
 
+
+-type angle_type() :: up | down | right | left.
 
 -record( piece, {
 	block1 :: #block{},
 	block2 :: #block{},
-	angle
+	angle = up :: angle_type()
 }).
 
 
@@ -43,6 +50,9 @@
 
 	user1_pid = undefined :: pid(),
 	user2_pid = undefined :: pid(),
+
+	user1_current_piece :: #piece{},
+	user2_current_piece :: #piece{},
 
 	user1_board :: #board{},
 	user2_board :: #board{}
--- server/src/DomainLogic/GameLogic/board.erl
+++ server/src/DomainLogic/GameLogic/board.erl
@@ -1,28 +1,56 @@
 -module(board).
 
--export([ new_empty/0 , get_block/3, set_block/4, remove_block/3 ]).
+-export([ new_empty/2 , get_all_blocks/1 ]).
+
+-export([ get_block/3, set_block/4, remove_block/3, is_valid_position/3 ]).
 
 -include("include/softstate.hrl").
 
 
-new_empty() ->
-	#board{}.
+new_empty( Width, Height ) ->
+	#board{ height = Height, width = Width }.
+
+
+
+get_all_blocks( Board = #board{} ) ->
+	gb_trees:values( Board#board.blocks ).
+
+
+
+is_valid_position( Board = #board{} , X , Y )
+				when Board#board.height > Y, Board#board.height > Y, Y >= 0, X >= 0 ->
+	true;
+is_valid_position( #board{} , _X , _Y ) ->
+	false.
+
+
+
 
 %returns the block record
-get_block( #board{ blocks = Blocks } , x , y ) ->
-	case gb_tree:lookup( {x,y}, Blocks) of
+get_block( #board{ blocks = Blocks } , X , Y ) ->
+	case gb_trees:lookup( {X,Y}, Blocks) of
 		none ->					empty;
 		{value, Block} ->		Block
 	end.
 
 
-%returns the new board record
-set_block( Block = #block{}, Board = #board{ blocks = Blocks } , x , y ) ->
-	Board#board{ blocks = gb_tree:insert({x,y}, Block , Blocks) }.
 
 
 %returns the new board record
-remove_block( Board = #board{ blocks = Blocks } , x , y ) ->
-	Board#board{ blocks = gb_tree:delete_any({x,y}, Blocks) }.
+%may throw out_of_bounds exception
+set_block( Block = #block{}, Board = #board{ blocks = Blocks } , X , Y )  ->
+	case is_valid_position( Board, X, Y) of 
+		true ->
+			Board#board{ blocks = gb_trees:insert({X,Y}, Block#block{ x = X , y = Y } , Blocks) };
+		false ->
+			throw( out_of_bounds )
+	end.
+
+
+
+
+%returns the new board record
+remove_block( Board = #board{ blocks = Blocks } , X , Y ) ->
+	Board#board{ blocks = gb_trees:delete_any({X,Y}, Blocks) }.
 
 
